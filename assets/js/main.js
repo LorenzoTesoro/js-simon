45967c33719cf26f643bdf5af4f0380c86ac7a83
LorenzoTesoro <lorenzotesoro2@gmail.com>
2022-10-19 14:05:36 +0200

    initial commit: setup

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+/* Consegna:
+Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi. Dopo 30 secondi i numeri scompaiono e l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt(). Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+*/
+
+// 1. Visualizzare in pagina 5 numeri casuali - mathRandom()
+
+// 2. Timer di 30 secondi, dopodichè i numeri scompaiono - setTimeout()
+
+// 3. L'utente deve inserire i numeri che ha visto - prompt() * 5
+
+// 4. Confronto tra numeri comparsi a schermo e numeri inseriti dall'utente - if() e ciclo for
