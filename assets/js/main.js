ba958c6c9acea5d713980dc6eee0c43ac1ddd5ef
LorenzoTesoro <lorenzotesoro2@gmail.com>
2022-10-19 15:13:15 +0200

    add promptGenerator

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,13 +30,27 @@ Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi. Do
 // 2. Timer di 30 secondi, dopodichè i numeri scompaiono - setTimeout()
     // 2.1 - Classe css d-none
     // 2.2 - dopo 30s applichi la classe a ul
-    
     const hideUlElement = function(){
         ulElement.classList.add("d_none");
     }
-
     setTimeout(hideUlElement, 3000);
 
 // 3. L'utente deve inserire i numeri che ha visto - prompt() * 5
+    // 3.1 - creo una funzione per stampare i 5 prompt
+    // 3.2 - setto un TimeOut 1s dopo la fine di quello precedente
+
+    function promptGenerator(){
+    
+    const prompts = [];
+    const n = 5
+
+        while(prompts.length < n){
+            const singlePrompt = Number(prompt("Inserisci un numero")) // genero un prompt
+            prompts.push(singlePrompt); // pusho il numero nell'array
+        }
+
+    }
+
+    setTimeout(promptGenerator, 4000);
 
 // 4. Confronto tra numeri comparsi a schermo e numeri inseriti dall'utente - if() e ciclo for
